6dc480a85815499afe01d5d19cf21c9db2db20ac
DerevenetsArtyom <fenix7036@gmail.com>
2015-10-17 16:28:22 +0300

    fixed vies for new files

--- students/views.py
+++ students/views.py
@@ -42,7 +42,18 @@ def students_delete(request, sid):
 
 
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    groups = (
+        {'id': 1,
+        'name': u'ДE-31',
+        'leader': { 'id' : 1,'name' : u'Деревенець Артем',}},
+        {'id': 2,
+        'name': u'ДE-32',
+        'leader': {"id" : 2,'name':u'Ильченко Анастасия',}},
+        {'id': 3,
+        'name': u'ДС-31',
+        'leader': {'id':3,'name':u'Орлов Евгений',}},
+        )
+    return render(request, 'students/groups_list.html',{'groups':groups})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
