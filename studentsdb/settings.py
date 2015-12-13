619e1d52de3618dc870480981bf3be986102899f
DerevenetsArtyom <fenix7036@gmail.com>
2015-12-13 20:45:51 +0200

    Add sorting by ID

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -77,12 +77,6 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR,'..', 'stud_db.sqlite3'),
-#     }
-# }
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
