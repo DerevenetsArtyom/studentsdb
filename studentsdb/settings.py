99ffc874a152bfa877b38f06215a07d680282bdb
DerevenetsArtyom <fenix7036@gmail.com>
2015-12-13 20:38:11 +0200

    Migrate to MySQL

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -77,11 +77,20 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR,'..', 'stud_db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'..', 'stud_db.sqlite3'),
-    }
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': 'localhost',
+            'USER': 'students_db_user',
+            'PASSWORD': 'password',
+            'NAME': 'students_db',
+        }
 }
 
 
