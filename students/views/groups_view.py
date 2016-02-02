626b6b20deab77318d4d49d9b79113fe4baa4e50
DerevenetsArtyom <fenix7036@gmail.com>
2016-02-03 01:31:31 +0200

    some corrections and comments

--- students/views/groups_view.py
+++ students/views/groups_view.py
@@ -27,13 +27,16 @@ def groups_list(request):
         # last page of results.
         groups = paginator.page(paginator.num_pages)
 
-    return render(request, 'students/groups_list.html',{'groups':groups})
+    return render(request, 'students/groups_list.html', {'groups': groups})
+
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
 
+
 def groups_edit(request, gid):
     return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
 
 def groups_delete(request, gid):
     return HttpResponse('<h1>Delete Group %s</h1>' % gid)
