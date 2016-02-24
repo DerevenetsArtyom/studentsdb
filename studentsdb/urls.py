4a51777a15566e090bbf23ce15df45911844a9e7
DerevenetsArtyom <fenix7036@gmail.com>
2016-02-24 17:58:53 +0200

    Set namespace for students.Move to students.urls

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,38 +1,16 @@
-"""studentsdb URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from .settings import MEDIA_ROOT, DEBUG
 
-from students.views.stud_view import StudentUpdateView, StudentDeleteView, StudentAddView
 from students.views.contact_admin import ContactView
 
 urlpatterns = patterns('',
 
-    # Students urls
     url(r'^$', 'students.views.stud_view.students_list', name='home'),
 
-    url(r'^students/add/$', StudentAddView.as_view(), name='students_add'),
-
-    url(r'^students/(?P<pk>\d+)/delete/$', StudentDeleteView.as_view(), name='students_delete'),
-
-    url(r'^students/(?P<sid>\d+)/journal/$', 'students.views.stud_view.students_journal', name='students_journal'),
-
-    url(r'^students/(?P<pk>\d+)/edit/$', StudentUpdateView.as_view(), name='students_edit'),
+    # Students urls with namespace
+    url(r'^students/', include('students.urls', namespace='students')),
 
     # Groups urls
     url(r'^groups/$', 'students.views.groups_view.groups_list', name='groups'),
@@ -53,9 +31,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     )
 
-
 if DEBUG:
     # serve files from media folder
     urlpatterns += patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': MEDIA_ROOT}))
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': MEDIA_ROOT})
+                            )
